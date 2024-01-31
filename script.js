3004296098e45091bd78f4c28264c58feb192cda
mortin0 <149053259+mortin0@users.noreply.github.com>
2024-01-31 20:03:29 +0100

    Using buttons for the choice. Creating a reset button and deleting all unnecessary log messages.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let container = document.querySelector('#container');
+const results = document.querySelector('#results');
+const resetButton = document.querySelector('#resetButton');
+
+
+//Initialise scores
+let playerScore = 0;
+let computerScore = 0;
+resetButton.addEventListener('click', resetGame);
+
 container.addEventListener('click', game);
 
 function getComputerChoice() {
@@ -22,19 +31,31 @@ function playRound(playerSelection, computerSelection) {
     else if(playerSelection == 'Scissors') result = computerSelection == 'Paper';
     else result = 3;
 
-    if (result == 2) endResult = 'Draw';
-    else if(result == 1) endResult = 'You Won! ' + playerSelection + ' beats ' + computerSelection;
-    else if(result == 0) endResult = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
-    else endResult = 'This input is not allowed!';
+    if (result == 2) {
+        endResult = 'Draw';
+    } else if (result == 1) {
+        endResult = `You Won! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+    } else if (result == 0) {
+        endResult = `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+    } else {
+        endResult = 'This input is not allowed!';
+    }
 
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(result);
     return endResult;
 }
 
 
-function game() 
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    results.textContent = 'Game reset!\nPlayer: 0 | Computer: 0';
+    container.addEventListener('click', game);
+}
+
+
+function game(event) 
 {
     let i = 0;
     let realComputerSelection;
@@ -61,14 +82,11 @@ function game()
             break;
         }
 
-        winner = playRound(realPlayerSelection, realComputerSelection);
-        if(winner != 'Draw' && winner != 'This input is not allowed!') i++;
-        console.log(winner);
-    
-    // realPlayerSelection = prompt("Let's play Rock, Paper, Scissors!");
-        
-    // //Make right string format
-    // realPlayerSelection = realPlayerSelection.charAt(0).toUpperCase() + realPlayerSelection.slice(1).toLowerCase();
+    winner = playRound(realPlayerSelection, realComputerSelection);        
+    results.textContent = `${winner}\nPlayer: ${playerScore} | Computer: ${computerScore}`;
 
-
+    if (playerScore === 5 || computerScore === 5) {
+        results.textContent += `\n${playerScore === 5 ? 'You' : 'Computer'} won the game!`;
+        container.removeEventListener('click', game);
+    }
 }
